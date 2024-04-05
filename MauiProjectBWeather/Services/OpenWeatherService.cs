6fc08d60402872e5ed537424d3935a398b85e609
torstenek123 <163210044+torstenek123@users.noreply.github.com>
2024-04-05 18:40:36 +0200

    Initial commit

---
+++ MauiProjectBWeather/Services/OpenWeatherService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json; 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+using MauiProjectBWeather.Models;
+
+namespace MauiProjectBWeather.Services
+{
+    public class OpenWeatherService
+    {
+        HttpClient httpClient = new HttpClient();
+        
+        //Your API Key
+        readonly string apiKey = "f86b8e5de4da7873c36cc9a5e4f05a93";
+
+        public async Task<Forecast> GetForecastAsync(string City)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+            return forecast;
+
+        }
+        public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+            return forecast;
+        }
+        private async Task<Forecast> ReadWebApiAsync(string uri)
+        {
+            //Read the response from the WebApi
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            WeatherApiData wd = await response.Content.ReadFromJsonAsync<WeatherApiData>();
+
+            //Object creation
+            var forecast = new Forecast();
+            forecast.City = wd.city.name;
+            forecast.Items = wd.list.Select(x => new ForecastItem { DateTime = UnixTimeStampToDateTime(x.dt), Temperature = x.main.temp, WindSpeed = x.wind.speed, Description = x.weather[0].description, Icon = $"https://openweathermap.org/img/w/{x.weather[0].icon}.png" }).ToList();
+            return forecast;
+        }
+        private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+}
